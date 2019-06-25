cba8c730d140c3d3af9ca676538147b33e92430c
Kevin Zou <kevinzou2150@gmail.com>
2019-06-24 22:35:35 -0400

    add doll type model, and set up many-to-one db relation between dolls and doll type category

--- dolldb/dolldb_index/models.py
+++ dolldb/dolldb_index/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class doll_type(models.Model):
+    name = models.CharField(max_length=32)
+    abbr = models.CharField(max_length=8)
+
 class doll(models.Model):
-    name
+    name = models.CharField(max_length=32)
+    type = models.ForeignKey(doll_type, on_delete=models.CASCADE)

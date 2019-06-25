590d71fc22c81ebb6f6f4f72548dd334c0d7c711
Kevin Zou <kevinzou2150@gmail.com>
2019-06-24 22:40:52 -0400

    add note for later

--- dolldb/dolldb_index/models.py
+++ dolldb/dolldb_index/models.py
@@ -9,3 +9,5 @@ class doll_type(models.Model):
 class doll(models.Model):
     name = models.CharField(max_length=32)
     type = models.ForeignKey(doll_type, on_delete=models.CASCADE)
+
+#possibly add choice field instead of doll_type

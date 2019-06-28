9c0ba074d6abcf87eb7923c4e859e67f11f6fc56
Kevin Zou <kevinzou2150@gmail.com>
2019-06-27 23:51:43 -0400

    redo model, change doll type from a model to a choice.

--- dolldb/dolldb_index/models.py
+++ dolldb/dolldb_index/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class doll_type(models.Model):
-    name = models.CharField(max_length=32)
-    abbr = models.CharField(max_length=8)
-
 class doll(models.Model):
-    name = models.CharField(max_length=32)
-    type = models.ForeignKey(doll_type, on_delete=models.CASCADE)
+    DOLL_TYPES = (
+        ('AR','AR'),
+        ('RF','RF'),
+        ('MG','MG'),
+        ('HG','HG'),
+        ('SG','SG'),
+        ('SMG','SMG'),
+    )
 
-#possibly add choice field instead of doll_type
+    name = models.CharField(max_length=32)
+    type = models.CharField(max_length=32, choices=DOLL_TYPES)

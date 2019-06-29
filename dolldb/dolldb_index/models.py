97444555b47e399834cf257ef0c7b8b088dba4d9
Kevin Zou <kevinzou2150@gmail.com>
2019-06-29 13:56:37 -0400

    add tile and skill data to model

--- dolldb/dolldb_index/models.py
+++ dolldb/dolldb_index/models.py
@@ -16,20 +16,34 @@ class doll(models.Model):
         (2,'★★'),
         (1,'⚝'),
     )
+    # doll data
     name = models.CharField(max_length=32)
     type = models.CharField(max_length=32, choices=DOLL_TYPES)
     rarity = models.PositiveSmallIntegerField(choices=RARITIES)
     index_id = models.PositiveSmallIntegerField()
     build_time = models.DurationField()
     
-    # stats
-    hp = models.IntegerField()
-    dmg = models.IntegerField()
-    rof = models.IntegerField()
-    acc = models.IntegerField()
-    eva = models.IntegerField()
-    arm = models.IntegerField()
-    ap = models.IntegerField()
+    # stat data
+    stat_hp = models.IntegerField()
+    stat_dmg = models.IntegerField()
+    stat_rof = models.IntegerField()
+    stat_acc = models.IntegerField()
+    stat_eva = models.IntegerField()
+    stat_armor = models.IntegerField()
+    stat_ap = models.IntegerField()
+    # tile data
+    tile_active = models.CharField(max_length=32)
+    tile_center = models.CharField(max_length=32)
+    tile_target = models.CharField(max_length=32)
+    tile_effect = models.CharField(max_length=128)
+    # skill data
+    skill_name = models.CharField(max_length=64)
+    skill_type = models.CharField(max_length=32)
+    skill_desc = models.TextField()
+    skill_target = models.CharField(max_length=32)
+    skill_icd = models.DurationField()
+    skill_cd = models.DurationField()
+    skill_data = models.TextField() #json until better solution
 
 class fairy(models.Model):
     FAIRY_TYPES = (
